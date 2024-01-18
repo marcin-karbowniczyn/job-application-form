fec3c3144e92e21861ddf74cbc7a0f2f71976325
Marcin <marcin.karbowniczyn@gmail.com>
2024-01-18 17:02:59 +0100

    Initial commit

---
+++ job_application/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Blueprint of Form table in the database
+# Jeśli chciałbym zmienić jakoś tę klasę, musiałbym ponownie wykonać komendy makemigration i migrate
+class Form(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField()
+    occupation = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
