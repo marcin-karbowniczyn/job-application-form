431f99c2c77269979702fe05684356b1253eea56
Marcin <marcin.karbowniczyn@gmail.com>
2024-01-19 13:57:06 +0100

    Set date picker to accept only dates starting from today, set email to be unique

--- job_application/models.py
+++ job_application/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Form(models.Model):
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     date = models.DateField()
     occupation = models.CharField(max_length=80)
 
--- job_application/views.py
+++ job_application/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from datetime import date
+
+import django.db.utils
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ApplicationForm
+from .models import Form
 
 
 # Request argument is being passed in the background by Django, when it calls this function
 # In project settings we can define in which directory templates should be store, but in default they are stored in the templates directory (which I created)
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            start_date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+
+            try:
+                Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=start_date, occupation=occupation)
+                messages.success(request, "Form submitted succesfully")
+            except django.db.utils.IntegrityError:
+                messages.error(request, "Candidate with this e-mail address has already been submitted.")
+            return redirect('index')
+
+    # Set date picker to only accept dates from today
+    date_picker_min = str(date.today())
+    return render(request, 'index.html', {'date_picker_min': date_picker_min})
